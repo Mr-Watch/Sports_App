59f411077fbd9e30b489bbad8e29e3a039a28850
akman15 <akisrocknroll@yahoo.gr>
2021-05-12 23:05:58 +0300

    Added proper firestore functionality

--- Sportspal/app/src/main/java/com/example/sportspal/ui/Firebase/AddMatch.java
+++ Sportspal/app/src/main/java/com/example/sportspal/ui/Firebase/AddMatch.java
@@ -9,8 +9,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -42,6 +44,11 @@ public class AddMatch extends Fragment {
     private EditText Date_textField;
     private EditText Sport_id_textField;
     private RadioGroup match_type_radioGroup;
+    private EditText Team_id_1;
+    private EditText Team_id_2;
+    private EditText Team_score_1;
+    private EditText Team_score_2;
+    private LinearLayout Single_player_layout;
 
     public AddMatch(){
 
@@ -57,64 +64,82 @@ public class AddMatch extends Fragment {
         Date_textField = view.findViewById(R.id.Date_textField);
         Sport_id_textField = view.findViewById(R.id.Sport_id_textField);
         match_type_radioGroup = view.findViewById(R.id.match_type_radioGroup);
+        Team_id_1=view.findViewById(R.id.Team1_id_number);
+        Team_id_2=view.findViewById(R.id.Team2_id_number);
+        Team_score_1=view.findViewById(R.id.Team1_score_number);
+        Team_score_2=view.findViewById(R.id.Team2_score_number);
+        Single_player_layout=view.findViewById(R.id.Single_player_layout);
+
         addMatch = view.findViewById(R.id.add_matches_button);
         addMatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String var_match_id = Match_id_textField.getText().toString();
-            String var_matchCity = City_textField.getText().toString();
-            String var_matchCountry = Country_textField.getText().toString();
-            int var_matchSport_id = Integer.parseInt(Sport_id_textField.getText().toString());
-            String var_matchDate = Date_textField.getText().toString();
+                String var_matchCity = City_textField.getText().toString();
+                String var_matchCountry = Country_textField.getText().toString();
+                int var_matchSport_id = Integer.parseInt(Sport_id_textField.getText().toString());
+                String var_matchDate = Date_textField.getText().toString();
 
-            int selected_type_id = match_type_radioGroup.getCheckedRadioButtonId();
-            RadioButton selected_type_button = getView().findViewById(selected_type_id);
-            String match_type = selected_type_button.getText().toString();
+                int selected_type_id = match_type_radioGroup.getCheckedRadioButtonId();
+                RadioButton selected_type_button = getView().findViewById(selected_type_id);
+                String match_type = selected_type_button.getText().toString();
 
-            try {
+                try {
 
-                Matches matches = new Matches();
-                matches.setCity(var_matchCity);
-                matches.setCountry(var_matchCountry);
-                matches.setSport_id(var_matchSport_id);
-                matches.setDate(var_matchDate);
-                if(match_type.equals("Single player")){
-                    Singleplayer singleplayer = new Singleplayer(matches,1,1);
-
-                    MainActivity.db.collection("Matches").document(""+var_match_id).set(singleplayer).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull @NotNull Task<Void> task) {
-                            Toast.makeText(getActivity(),"Match added.",Toast.LENGTH_LONG).show();
+                    Matches matches = new Matches();
+                    matches.setCity(var_matchCity);
+                    matches.setCountry(var_matchCountry);
+                    matches.setSport_id(var_matchSport_id);
+                    matches.setDate(var_matchDate);
+                    matches.setMatch_id(var_match_id);
+                    if(match_type.equals("Single player")){
+                        Singleplayer singleplayer = new Singleplayer(matches);
+                        for (int i=0;i<=Single_player_layout.getChildCount();i=i+2){
+                            View v1 = Single_player_layout.getChildAt(i);
+                            View v2 = Single_player_layout.getChildAt(i+1);
+                            EditText et1 = (EditText) v1;
+                            EditText et2 = (EditText) v2;
+                            if(et1 != null && et2 != null){
+                                singleplayer.add(Integer.parseInt(et1.getText().toString()),Integer.parseInt(et2.getText().toString()));}
                         }
-                    })
-                     .addOnFailureListener(new OnFailureListener() {
-                         @Override
-                         public void onFailure(@NonNull @NotNull Exception e) {
-                             Toast.makeText(getActivity(), "Match added.", Toast.LENGTH_LONG).show();
-                         }
-                     });
-                }else{
-                    Teambased teambased = new Teambased(matches,1,1,1,2);
+                        MainActivity.db.collection("Matches").document(""+var_match_id).set(singleplayer).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                Toast.makeText(getActivity(),"Match added.",Toast.LENGTH_LONG).show();
+                            }
+                        })
+                         .addOnFailureListener(new OnFailureListener() {
+                             @Override
+                             public void onFailure(@NonNull @NotNull Exception e) {
+                                 Toast.makeText(getActivity(), "Match added.", Toast.LENGTH_LONG).show();
+                             }
+                         });
+                    }else{
+                        int var_team_id_1 = Integer.parseInt(Team_id_1.getText().toString());
+                        int var_team_id_2 = Integer.parseInt(Team_id_2.getText().toString());
+                        int var_team_score_1 = Integer.parseInt(Team_score_1.getText().toString());
+                        int var_team_score_2 = Integer.parseInt(Team_score_2.getText().toString());
+                        Teambased teambased = new Teambased(matches,var_team_id_1,var_team_id_2,var_team_score_1,var_team_score_2);
 
-                    MainActivity.db.collection("Matches").document(""+var_match_id).set(teambased).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull @NotNull Task<Void> task) {
-                            Toast.makeText(getActivity(),"Match added.",Toast.LENGTH_LONG).show();
-                        }
-                    })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull @NotNull Exception e) {
-                                    Toast.makeText(getActivity(), "Match added.", Toast.LENGTH_LONG).show();
-                                }
-                            });
+                        MainActivity.db.collection("Matches").document(""+var_match_id).set(teambased).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                Toast.makeText(getActivity(),"Match added.",Toast.LENGTH_LONG).show();
+                            }
+                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull @NotNull Exception e) {
+                                        Toast.makeText(getActivity(), "Match added.", Toast.LENGTH_LONG).show();
+                                    }
+                                });
 
 
+                    }
+                }catch (Exception e){
+                    String message =e.getMessage();
+                        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
                 }
-            }catch (Exception e){
-                String message =e.getMessage();
-                    Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
-            }
 
         }
     });
